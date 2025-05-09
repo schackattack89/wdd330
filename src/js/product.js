aa8c2b986c495490a243f12a9fb123d2c4193358
schackattack89 <nschack@gmail.com>
2025-05-09 12:48:13 -0400

    wk2 team task complete

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,6 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+import productDetails from "./productDetails.mjs";
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
@@ -14,3 +15,8 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+const productId = getParam("product");
+productDetails(productId);
+
+console.log(findProductById(productId));
